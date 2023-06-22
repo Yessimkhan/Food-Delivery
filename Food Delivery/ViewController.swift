00fce81ee06d55dce0a7d005cec5e54ea351dfeb
Yessimkhan <200103408@stu.sdu.edu.kz>
2023-06-22 21:59:04 +0600

    Initial Commit

---
+++ Food Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Delivery
+//
+//  Created by Yessimkhan Zhumash on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
